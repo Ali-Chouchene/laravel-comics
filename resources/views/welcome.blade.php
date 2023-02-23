ae4309dc149c8403fb3b3b5d01b91156c804d18e
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-23 14:09:47 +0100

    fixing header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/js/app.js')
     <title>Document</title>
 </head>
@@ -13,14 +14,16 @@
 <body>
     <header>
         <div class="up-header">
-            <span>
-                <h5>DC POWER&#8480;VISA&#174;</h5>
-            </span>
-            <span>
-                <h5>
-                    ADDITIONAL DC SITES<i class="fa-solid fa-caret-down"></i>
-                </h5>
-            </span>
+            <div class="container row">
+                <span class="me-5">
+                    <h5>DC POWER&#8480;VISA&#174;</h5>
+                </span>
+                <span>
+                    <h5>
+                        ADDITIONAL DC SITES<i class="fa-solid fa-caret-down"></i>
+                    </h5>
+                </span>
+            </div>
         </div>
     </header>
 </body>
