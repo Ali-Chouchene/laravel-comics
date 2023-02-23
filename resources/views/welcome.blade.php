03b24ae31f861552153da7686862f2b22a9f5c9f
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-23 13:49:33 +0100

    start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/js/app.js')
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="up-header">
+            <span>
+                <h5>DC POWER&#8480;VISA&#174;</h5>
+            </span>
+            <span>
+                <h5>
+                    ADDITIONAL DC SITES<i class="fa-solid fa-caret-down"></i>
+                </h5>
+            </span>
+        </div>
+    </header>
+</body>
+
+</html>
